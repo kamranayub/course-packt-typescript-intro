ba1d3417b2d884d0aa0389c31bf59eef1ea21429
Kamran Ayub <kamran.ayub@gmail.com>
2016-11-21 10:29:16 -0600

    Better runtime example

--- AdvantagesAndDisadvantages/05-runtime.js
+++ AdvantagesAndDisadvantages/05-runtime.js
@@ -1,4 +1,11 @@
-var x = "foo";
-var y = "foo";
+function compare(x, y) {
+    if (typeof x === "number") {
+        return x === y ? 0 :
+            x > y ? 1 : -1;
+    } else if (typeof x === "string") {
+        return x.localCompare(y);
+    }
+    throw "Unknown types";
+}
 
-console.log(x.localCompare(y));
+console.log(compare(1, 2));
