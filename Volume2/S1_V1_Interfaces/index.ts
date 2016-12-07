233c8928214c1e05ab02674b5b993610e5ef137c
Kamran Ayub <kamran.ayub@gmail.com>
2016-12-06 19:37:39 -0600

    Add v2s1v1

---
+++ Volume2/S1_V1_Interfaces/index.ts
@@ -0,0 +1,17 @@
+import { IEncoder } from './IEncoder';
+import { base64encode } from './base64encoder';
+
+// store available encoders
+var encoders = {
+    base64: base64encode
+};
+
+// use base64 by default
+var encoder = encoders.base64;
+
+var input = "Hello World";
+var output = "SGVsbG8gV29ybGQ=";
+
+console.log("INPUT: ", input);
+console.log("OUTPUT:", encoder(input));
+console.log("EXPECT:", output);
