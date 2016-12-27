8dbb50f88df3755045cbcd8edc789d81a80e3bfe
Kamran Ayub <kamran.ayub@gmail.com>
2016-12-27 14:02:08 -0600

    Convert draggabilly to TS

---
+++ Volume3/S1_V3_Conversion/dist/draggabilly.d.ts
@@ -0,0 +1,16 @@
+/// <reference path="get-size.d.ts" />
+/// <reference path="unidragger.d.ts" />
+/*!
+ * Draggabilly v2.1.1
+ * Make that shiz draggable
+ * http://draggabilly.desandro.com
+ * MIT license
+ */
+declare var define: {
+    (dependencies: string[], factory: Function);
+    amd: boolean;
+};
+declare var module: {
+    exports: Object;
+};
+declare function require(dependency: string): any;
