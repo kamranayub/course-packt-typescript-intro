4f8919f9f9c530eb1c3baea009c9738645ef6fb4
Kamran Ayub <kamran.ayub@gmail.com>
2017-01-06 01:14:06 -0600

    Fix broadcast

--- Volume3/S2_V3_ServerSide/server/sockets.ts
+++ Volume3/S2_V3_ServerSide/server/sockets.ts
@@ -7,7 +7,6 @@ import * as Models from '../common/models'
 interface WhitelistedSocket extends SocketIO.Socket {
     emit(event: never, ...args: any[]): SocketIO.Socket;
     on(event: never, fn: Function): SocketIO.Socket;
-    broadcast: this;
 }
 
 /**
@@ -21,6 +20,7 @@ interface AppServerSocketEmitter extends WhitelistedSocket {
     emit(event: typeof SocketEvents.DELETED, data: Models.DeleteTodoCommand): SocketIO.Socket;
     emit(event: typeof SocketEvents.STATUSCHANGED, data: Models.ChangeTodoStatusCommand): SocketIO.Socket;
     emit(event: typeof SocketEvents.ALLSTATUSCHANGED, data: Models.ChangeTodoStatusCommand): SocketIO.Socket;
+    broadcast: AppServerSocketEmitter;
 }
 
 /**
@@ -33,6 +33,7 @@ interface AppServerSocketListener extends WhitelistedSocket {
     on(event: typeof SocketEvents.CHANGESTATUS, fn: (cmd: Models.ChangeTodoStatusCommand) => void): SocketIO.Socket;
     on(event: typeof SocketEvents.ALLCHANGESTATUS, fn: (cmd: Models.ChangeTodoStatusCommand) => void): SocketIO.Socket;    
     on(event: typeof SocketEvents.DISCONNECT, fn: Function): SocketIO.Socket;
+    broadcast: AppServerSocketEmitter;
 }
 
 /**
