cb95286478eceb94d39ace3caebf1402f877d7d5
Kamran Ayub <kamran.ayub@gmail.com>
2017-01-06 00:56:40 -0600

    Better interfaces

--- Volume3/S2_V3_ServerSide/server/sockets.ts
+++ Volume3/S2_V3_ServerSide/server/sockets.ts
@@ -2,13 +2,18 @@ import * as SocketEvents from '../common/events'
 import * as Models from '../common/models'
 
 /**
- * Server-side Socket.IO Socket emitter
+ * Overrides SocketIO.Socket that allows whitelisted function overloads
  */
-interface AppServerSocketEmitter extends SocketIO.Socket {
-    // "Override" usage of original on and emit to allow whitelisting events
+interface WhitelistedSocket extends SocketIO.Socket {
     emit(event: never, ...args: any[]): SocketIO.Socket;
     on(event: never, fn: Function): SocketIO.Socket;
+    broadcast: this;
+}
 
+/**
+ * Server-side Socket.IO Socket emitter
+ */
+interface AppServerSocketEmitter extends WhitelistedSocket {    
     emit(event: typeof SocketEvents.COUNT, data: { count: number }): SocketIO.Socket;
     emit(event: typeof SocketEvents.ALL, data: Models.Todo[]): SocketIO.Socket;
     emit(event: typeof SocketEvents.ADDED, data: Models.Todo): SocketIO.Socket;
@@ -16,25 +21,18 @@ interface AppServerSocketEmitter extends SocketIO.Socket {
     emit(event: typeof SocketEvents.DELETED, data: Models.DeleteTodoCommand): SocketIO.Socket;
     emit(event: typeof SocketEvents.STATUSCHANGED, data: Models.ChangeTodoStatusCommand): SocketIO.Socket;
     emit(event: typeof SocketEvents.ALLSTATUSCHANGED, data: Models.ChangeTodoStatusCommand): SocketIO.Socket;
-    broadcast: this;
 }
 
 /**
  * Server-side Socket.IO Socket listener
  */
-interface AppServerSocketListener extends SocketIO.Socket {
-    // "Override" usage of original on and emit to allow whitelisting events
-    emit(event: never, ...args: any[]): SocketIO.Socket;
-    on(event: never, fn: Function): SocketIO.Socket;
-
+interface AppServerSocketListener extends WhitelistedSocket {
     on(event: typeof SocketEvents.ADD, fn: (todo: Models.Todo) => void): SocketIO.Socket;
     on(event: typeof SocketEvents.EDIT, fn: (todo: Models.Todo) => void): SocketIO.Socket;
     on(event: typeof SocketEvents.DELETE, fn: (cmd: Models.DeleteTodoCommand) => void): SocketIO.Socket;
     on(event: typeof SocketEvents.CHANGESTATUS, fn: (cmd: Models.ChangeTodoStatusCommand) => void): SocketIO.Socket;
     on(event: typeof SocketEvents.ALLCHANGESTATUS, fn: (cmd: Models.ChangeTodoStatusCommand) => void): SocketIO.Socket;    
     on(event: typeof SocketEvents.DISCONNECT, fn: Function): SocketIO.Socket;
-
-    broadcast: AppServerSocketEmitter;
 }
 
 /**
